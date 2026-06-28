1a49bad12bb76f460c6ae2203456b3cc9ec41767
Ramtech2310 <24f1000536@ds.iitm.ac.in>
2026-06-28 18:37:47 +0530

    Completed Python Backend Hands-On 3

---
+++ PythonBackendFrameworks/hands_on_3/student_management/courses/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import CourseViewSet, StudentViewSet, EnrollmentViewSet
+
+router = DefaultRouter()
+router.register(r"courses", CourseViewSet)
+router.register(r"students", StudentViewSet)
+router.register(r"enrollments", EnrollmentViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
