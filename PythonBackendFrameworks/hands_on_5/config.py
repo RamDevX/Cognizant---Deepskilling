485491e35971e419e5d7ef1b270aa0a1ce185364
Ramtech2310 <24f1000536@ds.iitm.ac.in>
2026-07-01 09:02:02 +0530

    Completed Python Backend Hands on-5

--- PythonBackendFrameworks/hands_on_5/config.py
+++ PythonBackendFrameworks/hands_on_5/config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = "hands_on_5_secret_key"
 
     SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:Ram1234$@localhost/course_management_db"
+        "mysql+pymysql://root:####$@localhost/course_management_db"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
